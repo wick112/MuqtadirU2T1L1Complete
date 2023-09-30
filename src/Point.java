6e1092073ff0112e5921dcde3364299847e4c263
asir <asirm@nycstudents.net>
2023-09-30 10:47:01 -0400

    added quadrant method to point class added tester for point class

--- src/Point.java
+++ src/Point.java
@@ -3,6 +3,11 @@ public class Point {
     private int yCoord;
 
     //Constructors
+    public Point(int xVal, int yVal) {
+        xCoord = xVal;
+        yCoord = yVal;
+    }
+
     public Point(int val) {
         xCoord = val;
         yCoord = val;
@@ -31,5 +36,28 @@ public class Point {
 
     public String coordinate(){
         return "(" + xCoord + ", " + yCoord + ")";
+    }
+
+    public String quadrant() {
+        if (xCoord > 0 && yCoord > 0) {
+            return "I";
+        }
+        else if (xCoord < 0 && yCoord > 0) {
+            return "II";
+        }
+
+        else if (xCoord < 0 && yCoord < 0) {
+            return "III";
+        }
+        else if (xCoord > 0 && yCoord < 0) {
+            return "IV";
+        }
+        else if (xCoord == 0 && yCoord == 0) {
+            return "Origin" ;
+        }
+        else {
+            return "On an Axis" ;
+        }
+
     }
 }
